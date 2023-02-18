01107a321f78cc152b56902c1975ffe484f787eb
MEZ <mez@bitwormhole.com>
2023-02-18 13:00:02 +0800

    update to v0.0.11

--- module.go
+++ module.go
@@ -19,8 +19,8 @@ import (
 
 const (
 	theModuleName     = "github.com/bitwormhole/wpm"
-	theModuleVersion  = "v0.0.10"
-	theModuleRevision = 10
+	theModuleVersion  = "v0.0.11"
+	theModuleRevision = 11
 	theModuleResPath  = "src/main/resources"
 )
 
