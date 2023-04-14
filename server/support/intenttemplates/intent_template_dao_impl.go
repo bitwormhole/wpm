93daa84992da2259514330f90ae11e5248987354
MEZ <mez@bitwormhole.com>
2023-04-14 18:39:37 +0800

    实现了 IntentTemplate, Executable, ContentType  这三种对象修改图标

--- server/support/intenttemplates/intent_template_dao_impl.go
+++ server/support/intenttemplates/intent_template_dao_impl.go
@@ -127,6 +127,7 @@ func (inst *IntentTemplateDaoImpl) Update(id dxo.IntentTemplateID, o *entity.Int
 	}
 
 	m.Name = o.Name
+	m.IconURL = o.IconURL
 	m.Title = o.Title
 	m.Group = o.Group
 	m.Description = o.Description
